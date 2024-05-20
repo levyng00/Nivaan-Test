295b4e0670911557d70f02699fa84bc37fef6a22
firstPenguin1988 <tusharparmarofficial@gmail.com>
2024-05-20 11:04:24 +0530

    Created Navbar

---
+++ src/sections/Nav/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import NivaanLogo from '../../../public/desktop/nivaan-logo.webp'
+import NivaanLogoMob from '../../../public/mobile/nivaan-logo-mob.webp'
+import Image from 'next/image'
+import CustomButton from '@/components/CustomButton'
+
+const Navbar = () => {
+    const handleClick = () => console.log('Button Clicked !!!')
+  return (
+    <div className='flex flex-row justify-between py-3 md:py-6 border border-solid'>
+        <Image 
+            className='hidden md:flex' 
+            src={NivaanLogo} 
+            alt='Nivaan logo in desktop Navbar' 
+            width={231}
+            height={51}
+        />
+        <Image 
+            className='flex md:hidden' 
+            src={NivaanLogoMob} 
+            alt='Nivaan logo in Mobile Navbar' 
+            width={74.67}
+            height={16.67}
+        />
+      <CustomButton className='bg-[#DB5115]' title='Book Consultation' onBtnClick={handleClick} />
+    </div>
+  )
+}
+
+export default Navbar
