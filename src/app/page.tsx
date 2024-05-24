ad2cfad7766102b55a7dd8d85bfaa51b87149b6d
firstPenguin1988 <tusharparmarofficial@gmail.com>
2024-05-24 17:30:37 +0530

    Added some components like Faq,  FormAd & Herosection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import Faq from "@/sections/Faq/Faq";
 import Footer from "@/sections/Footer/Footer";
+import FormAd from "@/sections/FormAd/FormAd";
 import Navbar from "@/sections/Nav/Navbar";
+import HeroSection from "@/sections/Osteoarthritis/HeroSection";
 import OurCareExperts from "@/sections/OurCareExperts/OurCareExperts";
 import TreatmentAtNivaan from "@/sections/TreatmentAtNivaan/TreatmentAtNivaan";
 import WhatPatientSayAboutUs from "@/sections/WhatPatientSayAboutUs/WhatPatientSayAboutUs";
@@ -9,9 +12,12 @@ export default function Home() {
   return (
     <main className="text-4xl montserrat">
         <Navbar />
+        <HeroSection />
         <OurCareExperts />
         <TreatmentAtNivaan />
         <WhatPatientSayAboutUs />
+        <Faq />
+        <FormAd />
         <Footer />
     </main>
   );
---
+++ src/sections/Osteoarthritis/HeroSection.tsx
@@ -0,0 +1,46 @@
+import { montserratBold, rubikLight } from "@/app/fonts";
+import Form from "@/components/Form";
+import FormHeader from "@/components/FormHeader";
+import HeadMetricMob from "@/components/HeadMetricMob";
+import HeaderMetric from "@/components/HeaderMetric";
+import Image from "next/image";
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="bg-[#EAF1FB] pt-8 px-8 lg:px-0 ">
+      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-10">
+        <div className="flex-1">
+          <h1
+            className={`${montserratBold.className} text-[#2F438F] lg:text-5xl text-3xl`}
+          >
+            Non Surgical Osteoarthritis Treatment
+          </h1>
+          <ul
+            className={`list-disc lg:mt-14 mt-10 text-xl flex flex-col gap-2 ${rubikLight.className} px-8`}
+          >
+            <div className="flex items-center">
+              <li>4.8+ Rating on</li>
+              <Image
+                src={require("../../../public/googleImage.svg")}
+                alt="logo"
+                height={30}
+              />
+            </div>
+            <li>90% Surgeries Avoided</li>
+            <li>Insurance Support</li>
+          </ul>
+        </div>
+        <div className="flex-1 flex justify-center flex-col items-center gap-3">
+          <Form />
+        </div>
+      </div>
+      <div className="mt-8">
+        <HeaderMetric />
+        <HeadMetricMob />
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
