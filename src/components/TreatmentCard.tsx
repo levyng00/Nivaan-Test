245d0b4aba2e868a50f901e1c645814e43d7d43d
levyng00 <levyng.ng@gmail.com>
2024-05-24 14:33:54 +0600

    build error fix

--- src/components/TreatmentCard.tsx
+++ src/components/TreatmentCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Image, { StaticImageData } from 'next/image'
+import React from "react";
+import Image, { StaticImageData } from "next/image";
 
 interface Treatment {
   id: number;
@@ -15,25 +15,29 @@ interface TreatmentCardProps {
 
 const TreatmentCard: React.FC<TreatmentCardProps> = ({ treatment }) => {
   return (
-    <div className='p-6 bg-white rounded-lg md:w-[33%] w-full'>
-        <div className='flex justify-between'>
-            <h3 className='font-bold text-[16px] leading-[18px] md:text-[22px] md:leading-[26px] text-[#2F438F]'>{treatment.name}</h3>
-            <Image src={treatment.img} alt="treatement image" />
-        </div>
-        <div className='text-[12px] leading-[18px] md:text-[15px] md:leading-[22px] mt-4'>
-            <h4 className='text-[#2F438F] font-semibold'>Process:</h4>
-            <p>{treatment.process}</p>
-        </div>
-        <div className='text-[12px] leading-[18px] md:text-[15px] md:leading-[22px] mt-4'>
-            <h4 className='text-[#2F438F] font-semibold'>Benefits:</h4>
-            <ul>
-                {
-                    treatment.benefits.map(benefit => <li><span className='font-extrabold ml-0 md:ml-2'>•</span> {benefit}</li>)
-                }
-            </ul>
-        </div>
+    <div className="p-6 bg-white rounded-lg md:w-[33%] w-full">
+      <div className="flex justify-between">
+        <h3 className="font-bold text-[16px] leading-[18px] md:text-[22px] md:leading-[26px] text-[#2F438F]">
+          {treatment.name}
+        </h3>
+        <Image src={treatment.img} alt="treatement image" />
+      </div>
+      <div className="text-[12px] leading-[18px] md:text-[15px] md:leading-[22px] mt-4">
+        <h4 className="text-[#2F438F] font-semibold">Process:</h4>
+        <p>{treatment.process}</p>
+      </div>
+      <div className="text-[12px] leading-[18px] md:text-[15px] md:leading-[22px] mt-4">
+        <h4 className="text-[#2F438F] font-semibold">Benefits:</h4>
+        <ul>
+          {treatment.benefits.map((benefit, i) => (
+            <li key={i}>
+              <span className="font-extrabold ml-0 md:ml-2">•</span> {benefit}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TreatmentCard
+export default TreatmentCard;
