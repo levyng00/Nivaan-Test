aa2f988c886782b230ab4cc23a18c3638addc760
firstPenguin1988 <tusharparmarofficial@gmail.com>
2024-05-24 18:33:56 +0530

    Knee Page created with old assets

---
+++ src/app/knee-pain/page.tsx
@@ -0,0 +1,29 @@
+import HeaderMetric from "@/components/HeaderMetric";
+import Faq from "@/sections/Faq/Faq";
+import Footer from "@/sections/Footer/Footer";
+import FormAd from "@/sections/FormAd/FormAd";
+import HeroSection from "@/sections/KneePain/HeroSection";
+import Navbar from "@/sections/Nav/Navbar";
+import OurCareExperts from "@/sections/OurCareExperts/OurCareExperts";
+import TreatmentAtNivaan from "@/sections/TreatmentAtNivaan/TreatmentAtNivaan";
+import WhatPatientSayAboutUs from "@/sections/WhatPatientSayAboutUs/WhatPatientSayAboutUs";
+import React, { Suspense } from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Suspense>
+        <Navbar />
+        <HeroSection />
+        <OurCareExperts />
+        <TreatmentAtNivaan />
+        <WhatPatientSayAboutUs />
+        <Faq />
+        <FormAd />
+        <Footer />
+      </Suspense>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/sections/KneePain/HeroSection.tsx
@@ -0,0 +1,50 @@
+import { montserratBold, rubikLight } from "@/app/fonts";
+import Form from "@/components/Form";
+import FormHeader from "@/components/FormHeader";
+import HeadMetricMob from "@/components/HeadMetricMob";
+import HeaderMetric from "@/components/HeaderMetric";
+import Image from "next/image";
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="bg-[#EAF1FB] pt-8 px-8 lg:px-0 ">
+      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-10">
+        <div className="flex-1">
+          <h1
+            className={`${montserratBold.className} text-[#2F438F] lg:text-5xl text-3xl`}
+          >
+            Non Surgical Knee<br /> Pain Treatment
+          </h1>
+          <p className="text-[#DB5115] mt-4 text-sm lg:text-xl">
+            100% Insurance Coverage
+          </p>
+          <ul
+            className={`list-disc lg:mt-14 mt-10 text-xl flex flex-col gap-2 ${rubikLight.className} px-8`}
+          >
+            <div className="flex items-center">
+              <li>4.8+ Rating on</li>
+              <Image
+                src={require("../../../public/googleImage.svg")}
+                alt="logo"
+                height={30}
+              />
+            </div>
+            <li>90% Surgeries Avoided</li>
+            <li>Insurance Support</li>
+          </ul>
+        </div>
+        <div className="flex-1 flex justify-center flex-col items-center gap-3">
+          <FormHeader />
+          <Form />
+        </div>
+      </div>
+      <div className="mt-8">
+        <HeaderMetric />
+        <HeadMetricMob />
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
